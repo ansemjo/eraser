aec36f08b93aba3a7b68b6fcbfd12c6016d5523a
Anton Semjonov <anton@semjonov.de>
2018-12-15 03:24:21 +0100

    tidy up progress meter file, improve comments

--- eraser.go
+++ eraser.go
@@ -7,64 +7,52 @@ import (
 	"os"
 )
 
-func fatal(err string) {
-	if err != "" {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	os.Exit(1)
-}
-
-func min(a, b int64) int64 {
-	if a < b {
-		return a
-	}
-	return b
-}
-
 func main() {
 
+	// data source flags
 	flagZero := flag.Bool("zero", false, "use zeroes")
 	flagRand := flag.Bool("rand", true, "use random noise")
 
+	// custom usage message
 	flag.Usage = func() {
 		fmt.Println("Usage: $ eraser [options] filename")
 		flag.PrintDefaults()
 	}
 
+	// parse argv
 	flag.Parse()
 
 	// zero and rand cannot both be false
 	if !*flagRand && !*flagZero {
 		fmt.Fprintln(os.Stderr, "eraser: one of -rand or -zero required")
 		flag.Usage()
-		fatal("")
+		os.Exit(1)
 	}
 
 	// warn on unused flags
 	if len(flag.Args()) > 1 {
-		fmt.Printf("Warning: unused arguments: %s\n", flag.Args()[1:])
+		fmt.Fprintf(os.Stderr, "Warning: unused arguments: %s\n", flag.Args()[1:])
 	}
 
-	// open filename
+	// get filename
 	filename := flag.Arg(0)
 	if filename == "" {
 		fmt.Fprintln(os.Stderr, "eraser: target filename required")
 		flag.Usage()
-		fatal("")
+		os.Exit(1)
 	}
 
+	// open file for writing
 	file, err := os.OpenFile(filename, os.O_WRONLY, 0)
-	if err != nil {
-		fatal(err.Error())
-	}
+	check(err)
 	defer file.Close()
 
+	// get size
 	stat, err := file.Stat()
-	if err != nil {
-		fatal(err.Error())
-	}
+	check(err)
 	size := stat.Size()
 
+	// initialize desired reader
 	var reader io.Reader
 	if *flagZero {
 		reader = devZero()
@@ -72,18 +60,34 @@ func main() {
 		reader = devAES()
 	}
 
-	spinner := newProgressSpinner(size)
-	spinner.draw()
-	for spinner.current < size {
-		chunk := min(size-spinner.current, 256*1024)
+	// draw progress meter and copy chunks of data
+	meter := newProgress(size)
+	meter.draw()
+	for meter.current < size {
+		chunk := min(size-meter.current, 256*1024)
 		chunk, err = io.CopyN(file, reader, chunk)
-		if err != nil {
-			fatal(err.Error())
-		}
-		spinner.add(chunk)
+		check(err)
+		meter.add(chunk)
 	}
-	spinner.done()
+	meter.done()
 
+	// flush to disk
 	file.Sync()
 
+}
+
+// check for fatal errors
+func check(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+// return smaller number
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
 }
--- progress.go
+++ progress.go
@@ -5,63 +5,73 @@ import (
 	"time"
 )
 
-var spinRunes = []rune{'/', '-', '\\', '|'}
+type Spinner struct {
+	runes []rune
+	spin  int
+}
 
-const megabyte float32 = 1024 * 1024
+func newSpinner() *Spinner {
+	return &Spinner{[]rune{'/', '-', '\\', '|'}, 0}
+}
 
-type progressSpinner struct {
+func (s *Spinner) next() rune {
+	s.spin++
+	s.spin %= len(s.runes)
+	return s.runes[s.spin]
+}
+
+type Progress struct {
+	spin    *Spinner
+	avg     *Average
 	total   int64
 	current int64
 	fmt     string
-	spin    int
+	start   time.Time
 	last    time.Time
-	avg     *welfordAverage
 }
 
-func newProgressSpinner(total int64) *progressSpinner {
-	format := fmt.Sprintf("\r %%c %%%d.2f / %%.2f MiB", len(fmt.Sprintf("%.2f", float32(total)/megabyte)))
-	return &progressSpinner{
+func newProgress(total int64) *Progress {
+	return &Progress{
+		spin:    newSpinner(),
+		avg:     newAverage(),
 		total:   total,
 		current: 0,
-		fmt:     format,
-		spin:    0,
+		fmt:     fmt.Sprintf("\r %%c %%%dd / %%d", len(fmt.Sprint(total))),
+		start:   time.Now(),
 		last:    time.Now(),
-		avg:     newWelfordAverage(200),
 	}
 }
 
-func (sp *progressSpinner) draw() {
-	fmt.Printf(sp.fmt+" (%.2f MiB/s)",
-		spinRunes[sp.spin], float32(sp.current)/megabyte, float32(sp.total)/megabyte, sp.avg.avg/megabyte)
-	sp.spin = (sp.spin + 1) % len(spinRunes)
-	sp.last = time.Now()
+func (prg *Progress) draw() {
+	fmt.Printf(prg.fmt+" (%v, %.2f MiB/s)", prg.spin.next(),
+		prg.current, prg.total, time.Since(prg.start).Round(time.Millisecond), prg.avg.current/(1024*1024))
+	prg.last = time.Now()
 }
 
-func (sp *progressSpinner) add(b int64) {
-	sp.avg.sample(b)
-	sp.current += b
-	if time.Since(sp.last) > 80*time.Millisecond {
-		sp.draw()
+func (prg *Progress) done() {
+	fmt.Printf(prg.fmt+"\n", '✔', prg.current, prg.total)
+}
+
+func (prg *Progress) add(bytes int64) {
+	prg.avg.sample(bytes)
+	prg.current += bytes
+	if time.Since(prg.last) > time.Millisecond*83 {
+		prg.draw()
 	}
 }
 
-func (sp *progressSpinner) done() {
-	fmt.Printf(sp.fmt+"\n", '✔', float32(sp.current)/megabyte, float32(sp.total)/megabyte)
-}
-
-type welfordAverage struct {
-	avg     float32
-	samples float32
+type Average struct {
+	current float32
 	last    time.Time
 }
 
-func newWelfordAverage(samples float32) *welfordAverage {
-	return &welfordAverage{0, samples, time.Now()}
+func newAverage() *Average {
+	return &Average{0, time.Now()}
 }
 
-func (w *welfordAverage) sample(copied int64) {
-	delta := time.Since(w.last)
-	w.last = time.Now()
-	sample := float32(copied) / float32(delta.Seconds())
-	w.avg = w.avg + (sample-w.avg)/w.samples
+func (avg *Average) sample(added int64) {
+	delta := time.Since(avg.last)
+	avg.last = time.Now()
+	average := float32(added) / float32(delta.Seconds())
+	avg.current = avg.current + (average-avg.current)/200
 }
